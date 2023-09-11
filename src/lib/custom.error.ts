cb5befec6101cfc94ee1e701bf32a72e6cf37678
Mohan Vaddi <mohan.vaddi@inmar.com>
2023-09-11 16:59:11 +0530

    style: formatted using prettier and removed dump file

--- src/lib/custom.error.ts
+++ src/lib/custom.error.ts
@@ -1,6 +1,10 @@
 export class CustomError<T> extends Error {
   data: T;
-  constructor(message: string, public override name: string, data?: T) {
+  constructor(
+    message: string,
+    public override name: string,
+    data?: T
+  ) {
     super(message);
     this.data = data as T;
   }
